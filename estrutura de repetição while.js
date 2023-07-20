28cb35e74ac38c06cd818d9c6220167c7634352f
ihenriquex <ihenriquexo@gmail.com>
2023-07-20 11:00:28 -0300

    tipos de estrutura while

---
+++ estrutura de repetição while.js
@@ -0,0 +1,24 @@
+//estrutura de repetição - while(enquanto)
+//com numeros
+var valorInteiro=prompt("digite um valor")
+
+var contador=0
+while (contador<=valorInteiro) {
+    console.log(contador<=valorInteiro)
+console.log("repetição numero: ",contador)
+contador++
+}
+
+//exemplo de outra maneira
+//
+
+var desejaContinuar=true
+while (desejaContinuar) {
+    console.log("Oi")
+var inputUser=prompt("insira 1 se deseja parar")
+if(inputUser==1){
+    desejaContinuar=false
+}
+
+}
+//exercicio1
